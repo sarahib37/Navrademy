91335c9e6b57ee695bfb9e71289d6c00b5625bc8
SARAH <sarahigwe156@gmail.cocm>
2026-03-25 12:24:48 +0100

    dynamic routing for verify api

--- app/courses/api/auth/verify/route.ts
+++ app/courses/api/auth/verify/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/lib/firebaseAdmin";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
   const { token } = await req.json();
 
