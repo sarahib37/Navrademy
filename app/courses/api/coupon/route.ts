bc4bcf6b2504c1c94b1accbd2cff52424564f43f
SARAH <sarahigwe156@gmail.cocm>
2026-03-25 12:34:09 +0100

    Dynamic routing

--- app/courses/api/coupon/route.ts
+++ app/courses/api/coupon/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/lib/firebaseAdmin";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
     try {
       const { coupon } = await req.json();
--- app/courses/api/paystack/initiate/route.ts
+++ app/courses/api/paystack/initiate/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/firebaseAdmin";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
   try {
     const { coupon } = await req.json();
